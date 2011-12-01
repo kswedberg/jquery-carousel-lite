fa703a9ff6790d2a559248622e028c77c3c9ff2e
Karl Swedberg <kswedberg@gmail.com>
2011-11-30 22:58:54 -0500

    better support for partial visibility of items (e.g. {visible: 1.45}) made some variables more understandable

--- jcarousellite.js
+++ jcarousellite.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery jCarousellite Plugin v1.6.1
+ * jQuery jCarousellite Plugin v1.6.2
  *
  * Date: Sat Nov 19 23:55:07 2011 EST
  * Requires: jQuery v1.4+
@@ -17,7 +17,7 @@
 (function($) {
 
 $.jCarouselLite = {
-  version: '1.6.1',
+  version: '1.6.2',
   curr: 0
 };
 
@@ -36,16 +36,18 @@ $.fn.jCarouselLite = function(options) {
         ul = div.find('ul').eq(0),
         tLi = ul.children('li'),
         tl = tLi.length,
-        v = o.visible,
+        visibleNum = o.visible,
+        visibleCeil = Math.ceil(visibleNum),
+        visibleFloor = Math.floor(visibleNum),
         start = Math.min(o.start, tl - 1);
         direction = 1;
 
     div.data('dirjc', direction);
 
     if (o.circular) {
-        ul.prepend(tLi.slice(tl-v-1+1).clone(true))
-          .append(tLi.slice(0,v).clone(true));
-        start += v;
+        ul.prepend( tLi.slice(tl - visibleCeil - 1 + 1).clone(true) )
+          .append( tLi.slice(0,visibleCeil).clone(true) );
+        start += visibleCeil;
     }
     var li = ul.children('li'),
         itemLength = li.length,
@@ -56,9 +58,9 @@ $.fn.jCarouselLite = function(options) {
     div.css("visibility", "visible");
 
     if (o.autoCSS) {
-      li.css({overflow: o.vertical ? 'hidden' : 'visible', 'float': o.vertical ? 'none' : 'left'});
-      ul.css({margin: '0', padding: '0', position: 'relative', listStyleType: 'none', zIndex: 1});
       div.css({visibility: 'visible', overflow: 'hidden', position: 'relative', zIndex: 2, left: '0px'});
+      ul.css({margin: '0', padding: '0', position: 'relative', listStyleType: 'none', zIndex: 1});
+      li.css({overflow: o.vertical ? 'hidden' : 'visible', 'float': o.vertical ? 'none' : 'left'});
     }
 
     // Full li size(incl margin)-Used for animation
@@ -68,7 +70,7 @@ $.fn.jCarouselLite = function(options) {
     var ulSize = liSize * itemLength;
 
     // size of entire div(total length for just the visible items)
-    var divSize = liSize * v;
+    var divSize = liSize * visibleNum;
 
     if (o.autoCSS) {
       sizes[sizeProp] = divSize + 'px';
@@ -108,7 +110,7 @@ $.fn.jCarouselLite = function(options) {
             // set direction of subsequent scrolls to:
             //  1 if "btnNext" clicked
             // -1 if "btnPrev" clicked
-            div.data( 'dirjc', ((index) ? 1 : -1) );
+            div.data( 'dirjc', (index ? 1 : -1) );
           }
           return go( step );
         });
@@ -120,7 +122,7 @@ $.fn.jCarouselLite = function(options) {
         o.$btnPrev.addClass(o.btnDisabledClass);
       }
 
-      if ( o.btnNext && start + o.visible >= itemLength ) {
+      if ( o.btnNext && start + visibleFloor >= itemLength ) {
         o.$btnNext.addClass(o.btnDisabledClass);
       }
     }
@@ -128,14 +130,14 @@ $.fn.jCarouselLite = function(options) {
     if (o.btnGo) {
       $.each(o.btnGo, function(i, val) {
         $(val).bind('click.jc', function() {
-          return go(o.circular ? o.visible+i : i);
+          return go(o.circular ? visibleNum + i : i);
         });
       });
     }
 
     if (o.mouseWheel && div.mousewheel) {
       div.bind('mousewheel.jc', function(e, d) {
-        return d > 0 ? go(curr-o.scroll) : go(curr+o.scroll);
+        return d > 0 ? go(curr - o.scroll) : go(curr + o.scroll);
       });
     }
 
@@ -152,7 +154,7 @@ $.fn.jCarouselLite = function(options) {
     }
 
     function vis() {
-      return li.slice(curr).slice(0,v);
+      return li.slice(curr).slice(0, visibleCeil);
     }
 
     $.jCarouselLite.vis = vis;
@@ -163,36 +165,38 @@ $.fn.jCarouselLite = function(options) {
       if (o.beforeStart) {
         o.beforeStart.call(this, vis(), direction);
       }
-      // If circular we are in first or last, then goto the other end
+      // If circular and we are in first or last, then go to the other end
       if (o.circular) {
         // If first, then goto last
-        if (to <= start - v - 1) {
-          ul.css( animCss, -( (itemLength - (v*2) ) * liSize ) + "px" );
+        if (to <= start - visibleCeil - 1) {
+          ul.css( animCss, -( (itemLength - (visibleCeil * 2) ) * liSize ) + "px" );
           // If "scroll" > 1, then the "to" might not be equal to the condition; it can be lesser depending on the number of elements.
-          curr = to == start - v - 1 ? itemLength-(v*2) - 1 : itemLength-(v*2) - o.scroll;
-        } else if (to>=itemLength-v+1) { // If last, then goto first
-          ul.css(animCss, -( v * liSize ) + "px" );
+          curr = to == start - visibleCeil - 1 ? itemLength - (visibleCeil * 2) - 1 : itemLength - (visibleCeil * 2) - o.scroll;
+        } else if (to >= itemLength - visibleCeil + 1) { // If last, then go to first
+          ul.css(animCss, - ( visibleCeil * liSize ) + 'px' );
 
           // If "scroll" > 1, then the "to" might not be equal to the condition; it can be greater depending on the number of elements.
-          curr = (to == itemLength - v + 1) ? v + 1 : v + o.scroll;
+          curr = (to == itemLength - visibleCeil + 1) ? visibleCeil + 1 : visibleCeil + o.scroll;
         } else {
           curr = to;
         }
 
       // If non-circular and to points beyond first or last, we change to first or last.
       } else {
-        // Disable buttons when the carousel reaches the last/first, and enable when not
-        o.$btnPrev.toggleClass(o.btnDisabledClass, o.btnPrev && to <= 0);
-        o.$btnNext.toggleClass(o.btnDisabledClass, o.btnNext && to > itemLength - v);
 
         if (to < 0) {
           curr = 0;
-        } else if  (to > itemLength - v) {
-          curr = itemLength - v;
+        } else if  (to > itemLength - visibleFloor) {
+          curr = itemLength - visibleFloor;
         } else {
           curr = to;
         }
       }
+
+      // Disable buttons when the carousel reaches the last/first, and enable when not
+      o.$btnPrev.toggleClass(o.btnDisabledClass, o.btnPrev && curr === 0);
+      o.$btnNext.toggleClass(o.btnDisabledClass, o.btnNext && curr === itemLength - visibleFloor);
+
       $.jCarouselLite.curr = curr;
       running = true;
       aniProps[animCss] = -(curr * liSize);
